e43736d3741953aeb679d071df9edfd85df18220
vincent78-devweb <55990401+vincent78-devweb@users.noreply.github.com>
2020-01-15 10:34:08 +0100

    création timeline-manager

--- src/app/components/timeline/timeline-manager/timeline-manager.component.ts
+++ src/app/components/timeline/timeline-manager/timeline-manager.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router'
+
+import { TimelineService } from '../../../services/timeline/timeline.service';
+import { Timeline} from '../../models/timeline/timeline';
 
 @Component({
   selector: 'app-timeline-manager',
@@ -6,10 +11,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./timeline-manager.component.css']
 })
 export class TimelineManagerComponent implements OnInit {
+  
+  timeline: Timeline;
+  timelineForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private timelineService: TimelineService,
+    private route: ActivatedRoute,
+  ) { }
 
   ngOnInit() {
+    // Get recieved timeline id
+    this.route.paramMap.subscribe(params => {
+      this.timeline = this.timelineService.getTimeline( +params.get('timeline.id'));
+    });
+
+    // Initialize the timeline add form
+    this.timelineForm = new FormGroup({
+      name  : new FormControl('', [Validators.required, Validators.maxLength(60)])
+    });
   }
 
+  /**
+   * Generic error manager for the form controler
+   * @param controlName The name property of the field to control (see <mat-error> tag in the template for more details)
+   * @param errorName The name property of the error to control (see <mat-error> tag in the template for more details)
+   */
+  hasError = (controlName: string, errorName: string) =>{
+    return this.timelineForm.controls[controlName].hasError(errorName);
+  }
+
+  onSubmitTimeline(timeline) {
+    
+  }
 }
